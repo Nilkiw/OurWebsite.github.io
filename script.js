f04fd5c77d4c24c7c573a66d2a5edfb844602d32
Nilkiw <naluthgedona01@qub.ac.uk>
2015-11-30 15:11:24 +0000

    modified css

---
+++ script.js
@@ -0,0 +1,24 @@
+
+	var myCenter=new google.maps.LatLng(51.508742,-0.120850); /* Coordinates of London*/
+	var marker;
+
+	function initialize()  /* Function for google maps*/
+	{
+	var mapProp = {
+	  center:myCenter,
+	  zoom:2, /* Zoom settings*/
+	  mapTypeId:google.maps.MapTypeId.ROADMAP
+	  };
+
+	var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+	var marker=new google.maps.Marker({
+	  position:myCenter,
+	  animation:google.maps.Animation.BOUNCE
+	  });
+
+	marker.setMap(map);
+	}
+
+	google.maps.event.addDomListener(window, 'load', initialize);
+
